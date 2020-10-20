57afb75d7cf4496fa80425dead276106ca326f5a
Samyr Ribeiro <saamyr@live.com>
2020-10-20 18:28:04 -0300

    trabalhando com imagens e CSS no webpack

--- Webpack/Pratica1/webpack.config.js
+++ Webpack/Pratica1/webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
         filename:'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    mode:'development'
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader', 'css-loader']
+            },
+            {
+                test:/\.(png|jpg|gif|svg)$/,
+                use:['file-loader']
+            }
+        ]
+    }
 }
