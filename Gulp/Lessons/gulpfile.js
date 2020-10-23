f9f9284826a82e66c0f99a9992c1cb55ce21f2cf
Samyr Ribeiro <saamyr@live.com>
2020-10-23 18:50:34 -0300

    otimizando images e usando babel para js com gulp

--- Gulp/Lessons/gulpfile.js
+++ Gulp/Lessons/gulpfile.js
@@ -2,9 +2,14 @@ const {series, parallel, src, dest} = require('gulp');
 const rename = require('gulp-rename');
 const minifyJs = require('gulp-uglify');
 const minifyCss = require('gulp-clean-css');
+const minifyImage = require('gulp-image');
+const babel = require('gulp-babel');
 
 function javaScript(){
     return src('src/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
         .pipe(minifyJs())
         .pipe(rename({extname: '.min.js'}))
         .pipe(dest('dist/assets/js/'))
@@ -15,6 +20,12 @@ function css(){
     .pipe(rename({extname: '.min.css'}))
     .pipe(dest('dist/assets/style/'))
 }
+function images(){
+    return src('src/images/*')
+    .pipe(minifyImage())
+    .pipe(dest('dist/assets/images/'))
+}
 exports.js = javaScript;
 exports.css = css;
-exports.minify = parallel(javaScript, css)
+exports.images =  images;
+exports.minify = parallel(javaScript, css, images)
--- Gulp/Lessons/src/js/index.js
+++ Gulp/Lessons/src/js/index.js
@@ -1,2 +1,10 @@
 const text = 'Samyr'
 const hello = true
+
+const soma = () => {
+    let x = 1
+    let y = 2
+    let z = y - z
+    return `Resultado:${z}`
+}
+console.log(soma);
