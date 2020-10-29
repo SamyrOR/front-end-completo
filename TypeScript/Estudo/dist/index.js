5b73b8f16dd574793b08a7ba8dfde3bb1c2c8d19
Samyr O. Ribeiro <saamyr@live.com>
2020-10-28 23:10:22 -0300

    validaçao de forms com TS

--- TypeScript/Estudo/dist/index.js
+++ TypeScript/Estudo/dist/index.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 var nome = 'Samyr';
 var alo = 'Samyr';
 function hello(pessoa) {
-  return console.log('Hello ' + pessoa);
+    return console.log("Hello " + pessoa);
 }
 hello(nome);
-console.log('aaaaaaaaaaaaaaaaaaaaaaaa');
+console.log('aaaaaaaaaaaaaaaaaaaaaaaa ');
